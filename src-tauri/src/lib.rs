ec9171dd6dbebf65fd8e03f37c0a17902b200594
yuyeming0115 <1798966227@qq.com>
2026-06-26 00:53:43 +0800

    fix: minimal Tauri 2.x config - add build.rs, capabilities, lib.rs, plugins

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,20 @@
+use tauri::Manager;
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_shell::init())
+        .setup(|app| {
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from DotAgent!", name)
+}
